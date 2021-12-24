3a7daeb14713a68ba644f2e740f8758c4d18b224
Roberta <vcindustria@gmail.com>
2021-12-23 23:24:27 -0300

    Atualização

--- script.js
+++ script.js
@@ -3,4 +3,10 @@ function alterar() {
     document.getElementById("campo").value = "Trocou o campo!";
 }
 
-alterar()
+function somar(x, y) {
+    let total = x + y;
+
+    document.getElementById("campo").value = total;
+
+}
+var resultado = soma(10, 15);
